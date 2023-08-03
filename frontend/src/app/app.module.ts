2597313df5475c2d6170f6c6b7221c99fb1c46b6
Rodrigo Gomes <cklrod@hotmail.com>
2023-08-02 23:39:59 -0300

    criando o botão e navegação de product/create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,32 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./components/template/header/header.component";
 
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { HeaderComponent } from "./components/template/header/header.component";
 import { RodapeComponent } from "./components/template/rodape/rodape.component";
 import { NavComponent } from "./components/template/nav/nav.component";
 
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { HomeComponent } from "./views/home/home.component";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
-import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RodapeComponent, NavComponent, HomeComponent, ProductCrudComponent, ProductCreateComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RodapeComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    ProductCreateComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,7 +37,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
